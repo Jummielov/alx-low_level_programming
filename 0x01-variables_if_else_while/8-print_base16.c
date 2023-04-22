1110bb2a937923aa60c8a396fc7c343f29c93a98
Jummielov <adejumokeadewole@gmail.com>
2023-04-22 06:00:22 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,8 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 999999999ar((num % 10) + '0');
+	for (num = 0; num <= 9; num++)
+		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
