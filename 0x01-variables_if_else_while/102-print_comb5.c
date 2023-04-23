e112bf2afb87a383e5e0e379f609d5d5a07b58e4
Jummielov <adejumokeadewole@gmail.com>
2023-04-23 07:58:48 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,20 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < j && i != j)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i == 98 || j == 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		if (i == 98 || j == 99)
+		continue;
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
 return (0);
-}
