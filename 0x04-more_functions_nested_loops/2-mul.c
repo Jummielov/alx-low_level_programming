912277a904edf809509672870a6de434747b25c9
Jummielov <adejumokeadewole@gmail.com>
2023-04-27 19:16:03 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the product of a and b
  */
-void print_numbers(void)
+int mul(int a, int b)
 {
-	int _putchar(char i);
-
-	int i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n');
+	return (a * b);
 }
