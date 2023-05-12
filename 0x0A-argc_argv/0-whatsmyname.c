7e6b1a984151c6aa0a077b1d4c680dc623b4cf5d
Jummielov <adejumokeadewole@gmail.com>
2023-05-12 16:46:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-/** 
+/**
  * main - Program that prints its name, followed by a new line.
- * @argc: Represents the number of things we entered into the command prompt when running our executable
+ * @argc: Number of things we entered into the command prompt
  * @argv: An array of strings
  * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("Print the name of the program: %s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
