f699849d7fd03e7b8806e627ae2a269b71a9b7fb
Jummielov <adejumokeadewole@gmail.com>
2023-05-12 17:03:44 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-/** 
+/**
  * main -  Program that prints all arguments it receives.
- * @argc: Represents the number of things we entered into the command prompt when running our executable
+ * @argc:  The number of things we entered into the command prompt
  * @argv: An array of strings
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("argc = %d\n", argc);
-	printf("What is in argv[]:\n");
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
